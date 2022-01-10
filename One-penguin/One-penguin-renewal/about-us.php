cbaf77df41984b2cc4727eb807cbc1b123814eb9
mmmimo <gykyo0923@gmail.com>
2022-01-10 11:13:07 +0900

    img change

--- One-penguin/One-penguin-renewal/about-us.php
+++ One-penguin/One-penguin-renewal/about-us.php
@@ -119,6 +119,34 @@
         srcset="../images/1x/about-us-img7-3.png 1x,
         ../images/1x/about-us-img7-3.png 2x" alt="">
     </div>
+    <div class="page-sec">
+	
+	<li class="page-item servce"><a href="#"><img src="https://onepenguin.jp/wp-content/uploads/2020/07/service@2x-1.png" alt="写真"></a></li>
+		<li class="page-item about"><a href="#"><img src="https://onepenguin.jp/wp-content/uploads/2020/07/about@2x.png" alt="写真"></a></li>
+		<li class="page-item price"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/price@2x.png" alt="写真"></a></li>
+	<li class="page-item work"><a href=" " ><img src="https://onepenguin.jp/wp-content/uploads/2020/07/works@2x-1.png" alt="写真"></a></li>
+			<li class="page-item company"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/company@2x.png" alt="写真"></a></li>
+			<li class="page-item contact"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/contact@2x-2.png" alt="写真"></a></li>
+
+	
+	</div>
+
+<div class="page-sec2">
+	<div class="page-upper">
+
+	<li class="page-item2 servce"><a href="#"><img src="https://onepenguin.jp/wp-content/uploads/2020/07/service@2x-1.png" alt="写真"></a></li>
+		<li class="page-item2 about"><a href="#"><img src="https://onepenguin.jp/wp-content/uploads/2020/07/about@2x.png" alt="写真"></a></li>
+		<li class="page-item2 price"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/price@2x.png" alt="写真"></a></li>
+			
+	</div>
+		
+		<div class="page-lower">
+	<li class="page-item2 work"><a href=" " ><img src="https://onepenguin.jp/wp-content/uploads/2020/07/works@2x-1.png" alt="写真"></a></li>
+			<li class="page-item2 company"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/company@2x.png" alt="写真"></a></li>
+			<li class="page-item2 contact"><a href=" "><img src="https://onepenguin.jp/wp-content/uploads/2020/07/contact@2x-2.png" alt="写真"></a></li>
+	</div>
+	
+	</div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
