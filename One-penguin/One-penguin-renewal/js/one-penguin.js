4fb95c84b936c6b7ab5f0e11969cdb050ed7c804
mmmimo <gykyo0923@gmail.com>
2022-06-30 14:53:07 +0900

    6/30

--- One-penguin/One-penguin-renewal/js/one-penguin.js
+++ One-penguin/One-penguin-renewal/js/one-penguin.js
@@ -3,3 +3,20 @@ jQuery(window).on('load',function(){
     $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
     $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
   });
+
+  $(function() {  
+    $('.btn-6')
+      .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
